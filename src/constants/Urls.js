711aa9acdc6ac29dfef47100db6acf682ac3d16c
jjestrella <estrella.jjuan@gmail.com>
2022-06-01 15:02:46 -0700

    recommit

--- src/constants/Urls.js
+++
@@ -1,3 +0,0 @@
-export const Urls = {
-    PreventingLostTokens: 'https://docs.dnerochain.org/docs/preventing-lost-eth-erc20-tokens'
-};
--- src/state/types/DneroWallet.js
+++
@@ -1,3 +0,0 @@
-const name = "DNERO_WALLET";
-
-export const UPDATE_DNERO_WALLET_STATE = `${name}/UPDATE_DNERO_WALLET_STATE`;
