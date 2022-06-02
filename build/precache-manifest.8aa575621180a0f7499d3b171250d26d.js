009fc4142b470e1bcecffa602ccfa97bd054bb6f
jjestrella <estrella.jjuan@gmail.com>
2022-06-01 19:18:08 -0700

    build

---
+++ build/precache-manifest.8aa575621180a0f7499d3b171250d26d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852e9dc8836b371b56690bc84367c2a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8f3091e8e2169787657",
+    "url": "/static/css/main.dc86ce0f.chunk.css"
+  },
+  {
+    "revision": "03c39f5eab3c8e23d76a",
+    "url": "/static/js/2.dc6b853b.chunk.js"
+  },
+  {
+    "revision": "afb658385d29caa9989d813daf3df1ae",
+    "url": "/static/js/2.dc6b853b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f3091e8e2169787657",
+    "url": "/static/js/main.9bf51b73.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9bf51b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad55808e8246ca81e6e",
+    "url": "/static/js/runtime-main.caae8448.js"
+  }
+]);
